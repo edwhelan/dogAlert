53a71d28ff301b74e7b8eed2c3065606396ea531
Edward Whelan <vroxing@gmail.com>
2020-01-27 20:00:29 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using System;
+using RestSharp;
+using RestSharp.Authenticators;
+
+
+namespace HttpClientSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            var client = new RestClient("https://api.darksky.net/forecast/");
+            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest(Variables.WeatherApi + "/33.735284,-84.371662", Method.GET);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = client.Execute(request);
+            Console.WriteLine(response.Content); 
+        }
+
+        
+    }
+
+}
