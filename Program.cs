dc98833d362ba0b3f320fc2293228e36af8c8dcb
Edward Whelan <vroxing@gmail.com>
2020-01-31 08:57:07 -0500

    adds temp monitoring

--- Program.cs
+++ Program.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace Weather_Test
-{
-
-    
+{   
     class Program
     {
         static void Main()
         {
-            //return the payload from api
-            var payload = new DataFetch().GetPayload();
+            ////return the payload from api
+            //var payload = new DataFetch().GetPayload();
 
-            //initialize the payload for consumption
-            ForecastResponse forecastResponse = JsonConvert.DeserializeObject<ForecastResponse>(payload);
+            ////initialize the payload for consumption
+            //ForecastResponse forecastResponse = JsonConvert.DeserializeObject<ForecastResponse>(payload);
 
-            //get next 10 hours of weather data
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-                Console.WriteLine(forecastResponse.hourly.data[i].summary);
-            }
+            ////get next 10 hours of weather data
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    Console.WriteLine(forecastResponse.hourly.data[i].summary);
+            //}
 
-            //Console.WriteLine(forecastResponse.hourly.data[0].summary);
+            WeatherRating weatherRating = new WeatherRating();
+
+            weatherRating.WeatherRatingPrint();
+
         }
     }
-
 }
--- WeatherRating.cs
+++ WeatherRating.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace Weather_Test
 {
     public class WeatherRating
     {
         //consume data to get rating on safety
         //of leaving your dog outside in backyard
-
-
 
         //MVP qualifiers (dog stays inside)
         // too cold for doggos
@@ -15,8 +16,27 @@ namespace Weather_Test
         //too hot for doggos
         //weather > 80deg
         //weather average > 85deg if raining? [source this]
-        public WeatherRating()
+
+        public void WeatherRatingPrint()
         {
+            //return the payload from api
+            var payload = new DataFetch().GetPayload();
+
+            //initialize the payload for consumption
+            ForecastResponse forecastResponse = JsonConvert.DeserializeObject<ForecastResponse>(payload);
+            List<float> tempArray = new List<float>();
+            //get next 10 hours of weather data
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                tempArray.Add(forecastResponse.hourly.data[i].temperature);
+                Console.WriteLine(forecastResponse.hourly.data[i].summary);
+                Console.WriteLine(forecastResponse.hourly.data[i].temperature);
+
+            }
+            Console.WriteLine(tempArray.ToString());
         }
+
+        
     }
 }
